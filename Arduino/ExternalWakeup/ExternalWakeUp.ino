0e4fc8e9afaadc693e396a453074ede4c7784b5f
Eddy Tran <48467315+Dynasdragon@users.noreply.github.com>
2024-04-14 04:25:51 -0400

    Add files via upload

--- Arduino/ExternalWakeup/ExternalWakeUp.ino
+++ Arduino/ExternalWakeup/ExternalWakeUp.ino
@@ -57,29 +57,32 @@ source. They are pins: 0,2,4,12-15,25-27,32-39.
 //const char* password = "dynamo81";
 const char* ssid = "EDDYS-LAPTOP";
 const char* password = "eddyiscool";
-const IPAddress host(172,20,10,2); 
+//const IPAddress host(172,20,10,2); 
+//const IPAddress host (192,168,1,118);
+const IPAddress host (10,17,75,129);
 int port = 81;
 WiFiServer wifiServer(5005);
 
 RTC_DATA_ATTR int bootCount = 0;
+#define ackPin 14
 
 /*
 Method to print the reason by which ESP32
 has been awaken from sleep
 */
-void print_wakeup_reason(){
+bool print_wakeup_reason(){
   esp_sleep_wakeup_cause_t wakeup_reason;
 
   wakeup_reason = esp_sleep_get_wakeup_cause();
 
   switch(wakeup_reason)
   {
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
-    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); return true; break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); return true; break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); return true; break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); return true; break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); return true; break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); return false; break;
   }
 }
 
@@ -88,158 +91,190 @@ void setup(){
   Serial.begin(115200);
   delay(1000); //Take some time to open up the Serial Monitor
 
-  //Increment boot number and print it every reboot
-  ++bootCount;
-  Serial.println("Boot number: " + String(bootCount));
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1); //1 = High, 0 = Low
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG; 
+
+    pinMode(ackPin, OUTPUT);
+    digitalWrite(ackPin, LOW);
 
   //Print the wakeup reason for ESP32
-  print_wakeup_reason();
+  bool signalled = print_wakeup_reason();
+  if(signalled){
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
+    Serial.println("Boot number: " + String(bootCount));
 
-  // Initialize SPIFFS
-  if (!SPIFFS.begin(true)) {
-    Serial.println("Failed to mount file system");
-    return;
-  }
-  /*
-  First we configure the wake up source
-  We set our ESP32 to wake up for an external trigger.
-  There are two types for ESP32, ext0 and ext1 .
-  ext0 uses RTC_IO to wakeup thus requires RTC peripherals
-  to be on while ext1 uses RTC Controller so doesnt need
-  peripherals to be powered on.
-  Note that using internal pullups/pulldowns also requires
-  RTC peripherals to be turned on.
-  */
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_33,1); //1 = High, 0 = Low
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
 
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-
-  
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
-  
-
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 5;
-    config.fb_count = 1;
-  }
-        // Init Camera
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
-      Serial.printf("Camera init failed with error 0x%x", err);
+    // Initialize SPIFFS
+    if (!SPIFFS.begin(true)) {
+      Serial.println("Failed to mount file system");
       return;
     }
+    
+    
 
-  sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 2); // up the brightness just a bit
-    s->set_saturation(s, -1); // lower the saturation
-    s->set_contrast(s, 1);
-  }
-    // drop down frame size for higher initial frame rate
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    s->set_framesize(s, FRAMESIZE_SVGA);
-  }
+    if(psramFound()){
+      config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+        config.grab_mode = CAMERA_GRAB_LATEST;
 
-  #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-    s->set_vflip(s, 1);
-    s->set_hmirror(s, 1);
-  #endif
+    
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG; 
+    
 
-  #if defined(CAMERA_MODEL_ESP32S3_EYE)
-    s->set_vflip(s, 1);
-  #endif
-      WiFiClient client;
+    if(psramFound()){
+      config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+        config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.jpeg_quality = 5;
+      config.fb_count = 1;
+    }
+          // Init Camera
+      esp_err_t err = esp_camera_init(&config);
+      if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+      }
 
-  if (!client.connect(host, port)) {
-      Serial.println(client.connect(host,port));
-      Serial.println("Connection to host failed");
+    sensor_t * s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID) {
+      s->set_vflip(s, 1); // flip it back
+      s->set_brightness(s, 2); // up the brightness just a bit
+      s->set_saturation(s, -1); // lower the saturation
+      s->set_contrast(s, 1);
+    }
+      // drop down frame size for higher initial frame rate
+    if(config.pixel_format == PIXFORMAT_JPEG){
+      s->set_framesize(s, FRAMESIZE_VGA);
+    }
 
-      delay(1000);
-      return;
-  }
+    #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+      s->set_vflip(s, 1);
+      s->set_hmirror(s, 1);
+    #endif
 
-  Serial.println("Connected to server successful!");
+    #if defined(CAMERA_MODEL_ESP32S3_EYE)
+      s->set_vflip(s, 1);
+    #endif
+        WiFiClient client;
 
-  // capture camera frame
-  camera_fb_t *fb = esp_camera_fb_get();
-  if(!fb) {
-     Serial.println("Camera capture failed");
-      return;
-  } else {
-      Serial.println("Camera capture successful!");
-  }
+    int i = 0;
+    bool connected = false;
+    while(connected == false && i <= 5) {
+        int attempt = client.connect(host,port);
+        if(attempt != 0){
+          connected = true;
+          break;
+        }
+        i++;
+        delay(1000);
+    }
+    if(connected == false){
+        Serial.println("Connection to host failed");
+        Serial.println("Going to sleep now");
+        esp_deep_sleep_start();
+    }
 
-  const char *data = (const char *)fb->buf;
-  // Image metadata.  Yes it should be cleaned up to use printf if the function is available
-  Serial.print("Size of image:");
-  Serial.println(fb->len);
-  Serial.print("Shape->width:");
-  Serial.print(fb->width);
-  Serial.print("height:");
-  Serial.println(fb->height);
-  client.print("Shape->width:");
-  client.print(fb->width);
-  client.print("height:");
-  client.println(fb->height);
-  // Give the server a chance to receive the information before sending an acknowledgement.
-  delay(2000);
-  //getResponse(client, data);
-  Serial.print(data);
-  client.write(data, fb->len);
-  esp_camera_fb_return(fb);
 
-  Serial.println("Disconnecting...");
-  client.stop();
+    Serial.println("Connected to server successful!");
+    //Increment boot number and print it every reboot
+    ++bootCount;
+    digitalWrite(ackPin, HIGH);
+    delay(1000);
+    digitalWrite(ackPin, LOW);
 
-  delay(2000);
+    // capture camera frame
+    camera_fb_t *fb = esp_camera_fb_get();
+    if(!fb) {
+      Serial.println("Camera capture failed");
+        return;
+    } else {
+        Serial.println("Camera capture successful!");
+    }
+
+    const char *data = (const char *)fb->buf;
+    // Image metadata.  Yes it should be cleaned up to use printf if the function is available
+    Serial.print("Size of image:");
+    Serial.println(fb->len);
+    Serial.print("Shape->width:");
+    Serial.print(fb->width);
+    Serial.print("height:");
+    Serial.println(fb->height);
+
+    client.print("width:");
+    client.print(fb->width);
+    client.print("height:");
+    client.print(fb->height);
+    client.print("series:");
+    client.print(bootCount);
+    client.print("\r\n");
+    // Give the server a chance to receive the information before sending an acknowledgement.
+    delay(2000);
+    getResponse(client, "ack1");
+    Serial.print(data);
+    client.write(data, fb->len);
+    delay(1000);
+    client.print("tx_complete");
+    delay(2000);
+    esp_camera_fb_return(fb);
+
+    Serial.println("Disconnecting...");
+    client.stop();
+
+    delay(2000);
+    }
   }
 
   //Go to sleep now
   Serial.println("Going to sleep now");
   esp_deep_sleep_start();
   Serial.println("This will never be printed");
+}
+
+void getResponse(WiFiClient client, String msg) {
+  Serial.println("Waiting for Ack");
+  byte buffer[8] = { NULL };
+  while (client.available() > 0 || buffer[0] == NULL) {
+    int len = client.available();
+    if (len > 8) len = 4;
+    String response = client.readString();
+    if(response.indexOf(msg) != -1){
+      Serial.println("Got Ack");
+      break;
+    }
+  }
 }
 
 void loop(){
